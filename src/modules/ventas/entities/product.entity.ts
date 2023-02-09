9fb14611aee6ca8dbcca1b12f410f91600e2e29d
Joel7Anthony <anto777molina777@gmail.com>
2023-02-09 13:32:37 -0500

    cambio

--- src/modules/ventas/entities/product.entity.ts
+++ src/modules/ventas/entities/product.entity.ts
@@ -1,78 +1,84 @@
-import { BeforeInsert, BeforeUpdate, Column, CreateDateColumn, DeleteDateColumn,
-         Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
-import { CategoryEntity } from "./category.entity";
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { CategoryEntity } from './category.entity';
 
-@Entity('products', {schema:'ventas'})
+@Entity('products', { schema: 'ventas' })
+export class ProductEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+  @CreateDateColumn({
+    name: 'Create_date',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createAt: Date;
+  @UpdateDateColumn({
+    name: 'update_date',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updateAp: Date;
+  @DeleteDateColumn({
+    name: 'delete_date',
+    type: 'timestamp',
+    nullable: true,
+  })
+  deleteAp: Date;
 
-export class ProductEntity{
-    @PrimaryGeneratedColumn('uuid')
-    id:number
-    @CreateDateColumn({
-        name:'Create_date',
-        type:'timestamp',
-        default:() => 'CURRENT_TIMESTAMP',
-    })
-    createAt:Date;
-    @UpdateDateColumn({
-        name:'update_date',
-        type:'timestamp',
-        default:() => 'CURRENT_TIMESTAMP',
-    })
+  //Relations
+  @ManyToOne(() => CategoryEntity, (category) => category.product)
+  category: CategoryEntity;
 
-    updateAp:Date;
-    @DeleteDateColumn({
-        name:'delete_date',
-        type:'timestamp',
-        nullable:true,
-    })
-    deleteAp:Date;
-
-    //Relations
-    @ManyToOne (() => CategoryEntity, (category) => category.product)
-    category: CategoryEntity;
-
-    /*@ManyToOne (() => DetalleVentaEntity, (detalleVenta) => detallVenta.product)
+  /*@ManyToOne (() => DetalleVentaEntity, (detalleVenta) => detallVenta.product)
     detalleVenta: DetalleVentaEntity;*/
 
-    //Columnas
+  //Columnas
 
-    @Column('varchar', {
-        name:'title',
-        comment:'nombre del producto',
-    })
-    title:string;
-    @Column('number', {
-        name:'price',
-        comment:'precio con 2 decimales del producto'
-    })
-    price:number;
-    @Column('text', {
-        name:'Description',
-        comment: 'Descripcion del producto'
-    })
-    description:string;
-    @Column('array',{
-        name: 'images',
-        comment: 'imagenes del producto'
-    })
-    image:string[];
-    @Column('varchar',{
-        name:'category-id',
-        comment:'id de la categoria con la que se relaciona la tabla'
-    })
-    categoryId:'number'
+  @Column('varchar', {
+    name: 'title',
+    comment: 'nombre del producto',
+  })
+  title: string;
+  @Column('number', {
+    name: 'price',
+    comment: 'precio con 2 decimales del producto',
+  })
+  price: number;
+  @Column('text', {
+    name: 'Description',
+    comment: 'Descripcion del producto',
+  })
+  description: string;
+  @Column('array', {
+    name: 'images',
+    comment: 'imagenes del producto',
+  })
+  image: string[];
+  @Column('varchar', {
+    name: 'category-id',
+    comment: 'id de la categoria con la que se relaciona la tabla',
+  })
+  categoryId: 'number';
 
-    @BeforeInsert()
-    @BeforeUpdate()
-
-    async setTitle(){
-        if(!this.title){
-            return ('No hay prodcuto');
-        }
-        this.title = this.title.toUpperCase();
+  @BeforeInsert()
+  @BeforeUpdate()
+  async setTitle() {
+    if (!this.title) {
+      return 'No hay prodcuto';
     }
+    this.title = this.title.toUpperCase();
+  }
 
-    /*  @BeforeInsert()
+  /*  @BeforeInsert()
         @BeforeUpdate()
 
     async setEmail(){
@@ -82,7 +88,7 @@ export class ProductEntity{
         this.email = this.email.toLowerCase().trim();
     }*/
 
-    /*
+  /*
     @BeforeInsert()
     @BeforeUpdate()
 
